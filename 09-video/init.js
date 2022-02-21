c4c16fee916928521f72f8bff4bba907351f6933
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-02-21 23:41:51 +0600

    completed video project

--- 09-video/init.js
+++ 09-video/init.js
@@ -15,6 +15,6 @@ btn.addEventListener("click", function () {
 //preloader
 const preloader = document.querySelector(".preloader");
 
-window.DONContactLoaded("load", function () {
-  preloader.classList.add(".hide-preloader");
+window.addEventListener("load", function () {
+  preloader.classList.add("hide-preloader");
 });
