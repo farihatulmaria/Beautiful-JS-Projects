0e4ca4b5524230ba6bde287cd2ebfe9cdbfee0b3
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-02-25 17:28:57 +0600

    update

---
+++ 12-countdown/init.js
@@ -0,0 +1,64 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+// DOM
+
+const giveAway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+//seting the dymic giveaway time
+
+let futureDate = new Date(2022, 3, 25, 11, 30, 0);
+const year = futureDate.getFullYear();
+const hour = futureDate.getHours();
+const mins = futureDate.getMinutes();
+
+const month = months[futureDate.getMonth()];
+
+const date = futureDate.getDate();
+
+const weekday = weekdays[futureDate.getDay()];
+
+giveAway.textContent = `Giveaway Ends On : ${weekday} ${date} ${month} ${year} ${hour}:${mins}am `;
+
+// future time is ms
+
+const futureTime = futureDate.getTime();
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+
+  // 1s = 1000ms
+  // 1m = 60s
+  // 1hr = 60m
+  // 1d = 24hr
+
+  // value in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  let days = t / oneDay;
+}
+getRemainingTime();
