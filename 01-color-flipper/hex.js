1d00c7a6a1f927bcdaa28a15225e10bf4f58e9af
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2024-05-13 20:52:55 +0600

    update

--- 01-color-flipper/hex.js
+++ 01-color-flipper/hex.js
@@ -1,8 +1,15 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 const btn = document.getElementById("btn");
+const btnInput = document.getElementById("btn-input");
+const inputColor = document.getElementById("input-color");
 const color = document.querySelector(".color");
 
+
+btnInput.addEventListener("click", function () {
+  document.body.style.backgroundColor = inputColor.value;
+});
+ 
 btn.addEventListener("click", function () {
   let hexColor = "#";
   for (let i = 0; i < 6; i++) {
@@ -11,6 +18,7 @@ btn.addEventListener("click", function () {
   color.textContent = hexColor;
   document.body.style.backgroundColor = hexColor;
 });
+
  
 color.addEventListener("hover", function(){
   copyToClipboard(color.textContent)
